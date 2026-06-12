3b24d93990e08eca8185daebacf3fedd0e0fa2dc
tieburke <tiernanlburke@gmail.com>
2026-06-11 21:09:28 -0600

    Move anon key to environment variable

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://saaznlfyoxibflriyiil.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhYXpubGZ5b3hpYmZscml5aWlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MjEwMDEsImV4cCI6MjA5NjA5NzAwMX0.QIVIP_cvoOHtU_WCg5S8WYQZ0MZyfGh8yDDd1cWtQmw";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
